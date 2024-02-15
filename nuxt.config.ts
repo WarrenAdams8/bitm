83b844bcd3a272583d3665aba2becba69c4e4aaa
WarrenAdams8 <warren.codes@outlook.com>
2024-02-15 12:40:12 -0500

    .
    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@hebilicious/vue-query-nuxt", "@vueuse/nuxt", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  modules: [
+    "@hebilicious/vue-query-nuxt",
+    "@vueuse/nuxt",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+  ],
   typescript: {
     strict: true,
   },
